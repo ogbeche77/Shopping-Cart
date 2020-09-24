6e7ed86d9901518a63ec2e93b4928eb8cd9bce76
ogbeche77 <ogbeche77@yahoo.com>
2020-09-24 12:52:11 +0200

    Style code refactored

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import style from './Cart.module.css';
 import formatCurrency from "../util";
 import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
 import { connect } from 'react-redux';
+import Modal from 'react-modal';
 import { removeFromCart } from '../actions/cartActions';
-
+import { createOrder, clearOrder } from '../actions/orderActions';
 class Cart extends Component {
     constructor(props) {
         super(props);
@@ -23,24 +26,67 @@ class Cart extends Component {
             name: this.state.name,
             email: this.state.email,
             address: this.state.address,
-            cartItems: this.props.cartItems
+            cartItems: this.props.cartItems,
+            total: this.props.cartItems.reduce((a, c) => a + c.price * c.count, 0),
         };
         this.props.createOrder(order);
+    };
+    closeModal = () => {
+        this.props.clearOrder();
     }
     render() {
-        const { cartItems } = this.props;
+        const { cartItems, order } = this.props;
         return (
             <div>
                 {cartItems.length === 0 ? (
-                    <div className="cart cart-header">Cart is empty</div>
+                    <div className={style.cart}>Cart is empty</div>
                 ) : (
-                        <div className="cart cart-header">You have {cartItems.length} in the cart {" "}
+                        <div className={style.cart}>You have {cartItems.length} in the cart {" "}
                         </div>
                     )}
+                {order && <Modal isOpen={true} onRequestClose={this.closeModal}>
+                    <Zoom>
+                        <button className={style.closeModal} onClick={this.closeModal}>x</button>
+                        <div className={style.orderDetails}>
+                            <h3 className={style.successMessage}> Your order has been placed</h3>
+                            <h2>Order {order._id}</h2>
+                            <ul>
+                                <li>
+                                    <div>Name:</div>
+                                    <div>{order.name}</div>
+                                </li>
+                                <li>
+                                    <div>Email:</div>
+                                    <div>{order.email}</div>
+                                </li>
+                                <li>
+                                    <div>Address:</div>
+                                    <div>{order.address}</div>
+                                </li>
+                                <li>
+                                    <div>Date:</div>
+                                    <div>{order.createdAt}</div>
+                                </li>
+                                <li>
+                                    <div>Total:</div>
+                                    <div>{formatCurrency(order.total)}</div>
+                                </li>
+                                <li>
+                                    <div>Cart Items:</div>
+                                    <div>{order.cartItems.map(x => (
+                                        <div> {x.count} * {x.title}</div>
+                                    ))}</div>
+                                </li>
+                            </ul>
+                        </div>
+                    </Zoom>
+
+
+                </Modal>}
                 <div>
-                    <div className="cart">
+                    <div className={style.cart}>
                         <Fade left cascade>
-                            <ul className="cart-items">
+                            <ul className={style.cartItems}>
                                 {cartItems.map(item => (
                                     <li key={item._id}>
                                         <div>
@@ -48,9 +94,9 @@ class Cart extends Component {
                                         </div>
                                         <div>
                                             <div>{item.title}</div>
-                                            <div className="right">
+                                            <div className={style.right}>
                                                 {formatCurrency(item.price)} x {item.count}{"  "}
-                                                <button className="button primary" onClick={() => this.props.removeFromCart(item)}>
+                                                <button className={style.button} onClick={() => this.props.removeFromCart(item)}>
                                                     Remove
                                             </button>
                                             </div>
@@ -62,8 +108,8 @@ class Cart extends Component {
                     </div>
                     {cartItems.length !== 0 && (
                         <div>
-                            <div className="cart">
-                                <div className="total">
+                            <div className={style.cart}>
+                                <div className={style.total}>
                                     <div>
                                         Total: {" "}
                                         {formatCurrency(cartItems.reduce((a, c) => a + c.price * c.count, 0)
@@ -71,16 +117,16 @@ class Cart extends Component {
                                     </div>
                                     <button onClick={() => {
                                         this.setState({ showCheckout: true });
-                                    }} className="button primary">
+                                    }} className={style.button}>
                                         Proceed
                             </button>
                                 </div>
                             </div>
                             {this.state.showCheckout && (
                                 <Fade right cascade>
-                                    <div className="cart">
+                                    <div className={style.cart}>
                                         <form onSubmit={this.createOrder}>
-                                            <ul className="form-container">
+                                            <ul className={style.formContainer}>
                                                 <li>
                                                     <label>Email</label>
                                                     <input name="email" type="email" required onChange={this.handleInput}>
@@ -102,7 +148,7 @@ class Cart extends Component {
                                                     </input>
                                                 </li>
                                                 <li>
-                                                    <button className="button primary" type="submit">Checkout</button>
+                                                    <button className={style.button} type="submit">Checkout</button>
                                                 </li>
 
                                             </ul>
@@ -122,7 +168,8 @@ class Cart extends Component {
 }
 
 export default connect((state) => ({
+    order: state.order.order,
     cartItems: state.cart.cartItems,
 }),
-    { removeFromCart }
+    { removeFromCart, createOrder, clearOrder }
 )(Cart);
